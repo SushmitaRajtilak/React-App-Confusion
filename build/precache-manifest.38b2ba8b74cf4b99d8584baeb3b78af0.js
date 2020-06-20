e5a664969b9a02fff72574cb379f72a526a66e9d
Sushmita Rajtilak <sushmita7898@gmail.com>
2020-06-21 00:17:34 +0530

    NPM Build

---
+++ build/precache-manifest.38b2ba8b74cf4b99d8584baeb3b78af0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a270d6a9d8edf58b95497c4c66ad3073",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71219af615aed00d7199",
+    "url": "/static/css/2.c21e1fb8.chunk.css"
+  },
+  {
+    "revision": "1c4c6a4eedd4bd1adaab",
+    "url": "/static/css/main.85cd3eb7.chunk.css"
+  },
+  {
+    "revision": "71219af615aed00d7199",
+    "url": "/static/js/2.b2faba7c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.b2faba7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c4c6a4eedd4bd1adaab",
+    "url": "/static/js/main.dd605121.chunk.js"
+  },
+  {
+    "revision": "84926f692196015996db",
+    "url": "/static/js/runtime-main.c71f3b90.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
